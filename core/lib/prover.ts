993f134be6b84dca852572b91e246b7a2a719fd7
qiaomengnan <qiaomengnan@ypshengxian.com>
2023-04-19 15:17:01 +0800

    fix: Prover support for web

--- core/lib/prover.ts
+++ core/lib/prover.ts
@@ -1,4 +1,6 @@
 import {pathJoin} from "./utils";
+import {any} from "hardhat/internal/core/params/argumentTypes";
+import window = Mocha.reporters.Base.window;
 
 const main_update_state_js_wc = require('../../circuits/main_update_state_js/witness_calculator');
 const main_withdraw_js_wc = require('../../circuits/main_withdraw_js/witness_calculator');
@@ -7,7 +9,16 @@ const fs = require("fs");
 const snarkjs = require("snarkjs");
 
 export class Prover {
+
     static async updateState(circuitPath: string, input: any) {
+        if (typeof window == 'object') {
+            return await Prover.updateStateBS('', '', any)
+        } else {
+            return await Prover.updateStateNode(circuitPath, input)
+        }
+    }
+
+    static async updateStateNode(circuitPath: string, input: any) {
         let wasm = pathJoin([circuitPath, "main_update_state_js", "main_update_state.wasm"]);
         let zkey = pathJoin([circuitPath, "circuit_final.zkey.16"]);
         const wc = require(`${circuitPath}/main_update_state_js/witness_calculator`);
@@ -34,6 +45,14 @@ export class Prover {
     }
 
     static async verifyState(circuitPath: string, proofAndPublicSignals: any) {
+        if (typeof window == 'object') {
+            return await Prover.verifyStateBS('', proofAndPublicSignals)
+        } else {
+            return await Prover.verifyStateNode(circuitPath, proofAndPublicSignals)
+        }
+    }
+
+    static async verifyStateNode(circuitPath: string, proofAndPublicSignals: any) {
         const proof = proofAndPublicSignals.proof;
         const publicSignals = proofAndPublicSignals.publicSignals;
 
@@ -48,6 +67,14 @@ export class Prover {
     }
 
     static async withdraw(circuitPath: string, input: any) {
+        if (typeof window == 'object') {
+            return await Prover.withdrawBS('', '', input)
+        } else {
+            return await Prover.withdrawNode(circuitPath, input)
+        }
+    }
+
+    static async withdrawNode(circuitPath: string, input: any) {
         let wasm = pathJoin([circuitPath, "main_withdraw_js", "main_withdraw.wasm"]);
         let zkey = pathJoin([circuitPath, "circuit_final.zkey.14"]);
         const wc = require(`${circuitPath}/main_withdraw_js/witness_calculator`);
@@ -73,7 +100,17 @@ export class Prover {
         return Prover.buildProofAndPublicSignals(main_withdraw_js_wc, wasmPath, zkeyPath, input)
     }
 
+
     static async verifyWithrawal(circuitPath: string, proofAndPublicSignals: any) {
+        if (typeof window == 'object') {
+            return await Prover.verifyWithrawalBS('', proofAndPublicSignals)
+        } else {
+            return await Prover.verifyWithrawalNode(circuitPath, proofAndPublicSignals)
+        }
+    }
+
+
+    static async verifyWithrawalNode(circuitPath: string, proofAndPublicSignals: any) {
         const proof = proofAndPublicSignals.proof;
         const publicSignals = proofAndPublicSignals.publicSignals;
 
